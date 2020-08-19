7f2536be70612c47ebe168624ca319b73c474023
ThayPedroso <thayla.pedroso88@gmail.com>
2020-08-18 23:54:53 -0300

    Update App.js
    allFunctions, but broken interface..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from "react";
 
 import "./styles.css";
+import { useState, useEffect } from "react";
+
+import api from './services/api'
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = api.post('repositories', {
+      "title": "Tindev",
+      "url": "github.com/Tindev",
+      "techs": [
+        "NodeJS",
+        "React",
+        "ReactNative"
+      ]
+    })
+
+    const repo = response.data
+
+    setRepositories(...repositories, repo)
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`)
+
+    setRepositories(...repositories)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo =>{ return (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        )})}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
